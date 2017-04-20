cfbf27675d2f5a65648e1aa522c59538b8c5cd28
kirky <brentckirkland@gmail.com>
2017-04-20 11:28:23 -0700

    working

---
+++ index.js
@@ -0,0 +1,36 @@
+const Datastore = require('@google-cloud/datastore');
+const projectId = 'slurp-165217';
+const datastore = Datastore({
+  projectId: projectId
+});
+/**
+ * Background Cloud Function to be triggered by Pub/Sub.
+ *
+ * @param {object} event The Cloud Functions event.
+ * @param {function} The callback function.
+ */
+exports.storeSoilMeasures = function helloPubSub (event, callback) {
+  const pubsubMessage = event.data;
+
+  const kind = 'soil_measure'
+  const name = event.eventId
+  const taskKey = datastore.key([kind, name]);
+
+  const task = {
+    key: taskKey,
+    data: {
+      device_id: pubsubMessage.attributes.device_id,
+      data: Buffer.from(pubsubMessage.data, 'base64').toString(),
+      published_at: pubsubMessage.attributes.published_at
+    }
+  };
+
+  datastore.save(task)
+  .then(() => {
+    console.log(`Saved ${task.key.name}`);
+  });
+
+
+
+  callback();
+};
